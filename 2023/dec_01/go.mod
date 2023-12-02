02e57ea0695c96597431397bc683be795a5d04de
David Young <david@nova-labs.com>
2023-12-01 23:09:47 -0800

    Adds dec 1st

---
+++ 2023/dec_01/go.mod
@@ -0,0 +1,3 @@
+module dec_01
+
+go 1.21.4
---
+++ 2023/dec_01/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Result struct {
+	Sub string
+	Dig int
+	Idx int
+}
+
+func pt1() {
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		digits := []rune{}
+		for _, r := range s {
+			if r <= 57 {
+				digits = append(digits, r)
+			}
+		}
+		vAsStr := string([]rune{digits[0], digits[len(digits)-1]})
+		v, err := strconv.Atoi(vAsStr)
+		check(err)
+		total += v
+	}
+	check(scanner.Err())
+
+	fmt.Println(total)
+}
+
+func pt2() {
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		s := scanner.Text()
+		results := []Result{}
+		for dig, sub := range []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"} {
+			if firstIdx := strings.Index(s, sub); firstIdx >= 0 {
+				results = append(results, Result{sub, dig, firstIdx})
+				if lastIdx := strings.LastIndex(s, sub); lastIdx != firstIdx {
+					results = append(results, Result{sub, dig, lastIdx})
+				}
+			}
+			if firstIdx := strings.Index(s, fmt.Sprint(dig)); firstIdx >= 0 {
+				results = append(results, Result{fmt.Sprint(dig), dig, firstIdx})
+				if lastIdx := strings.LastIndex(s, fmt.Sprint(dig)); lastIdx != firstIdx {
+					results = append(results, Result{fmt.Sprint(dig), dig, lastIdx})
+				}
+			}
+		}
+		sort.SliceStable(results, func(i, j int) bool {
+			return results[i].Idx <= results[j].Idx
+		})
+		vAsStr := fmt.Sprint(results[0].Dig) + fmt.Sprint(results[len(results)-1].Dig)
+		v, err := strconv.Atoi(vAsStr)
+		check(err)
+		total += v
+	}
+	check(scanner.Err())
+
+	fmt.Println(total)
+
+}
+
+func main() {
+	pt1()
+	pt2()
+}
